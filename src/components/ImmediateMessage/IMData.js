5bac28098c613b1ffe51338d567c43c21b93a191
SG\tony.yang <tony.yang@bwoil.com>
2018-02-08 22:33:34 +0800

    change min.js  adjust

--- src/components/ImmediateMessage/IMData.js
+++ src/components/ImmediateMessage/IMData.js
@@ -1,8 +1,18 @@
-import {observable} from 'mobx';
+import { observable,  computed, action } from "mobx";
 import {FloatDialogController} from "../FloatDialog/FloatDialogController";
 
 export class IMData {
+
     @observable status = 1;
     @observable mediumStatus = false;
     @observable floatController = new FloatDialogController();
+
+    @computed get setIconStyle() {
+         return{fontSize: 16, color: '#08c', marginLeft: 16, marginRight: 16} ;
+    }
+
+    @action.bound openContectList(event) {
+         this.status =this.status === 1?2:1 ;
+    }
+
 }
--- src/components/Min/Min.js
+++ src/components/Min/Min.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {action} from 'mobx';
 import {observer, inject} from 'mobx-react';
 import {Icon} from 'antd';
 import './Min.less';
@@ -14,20 +13,11 @@ export class Min extends React.Component {
         console.log('imdata', this.imdata);
     }
 
-    @action.bound
-    change() {
-        if (this.imdata.status === 1) {
-            this.imdata.status = 2;
-        } else {
-            this.imdata.status = 1;
-        }
-    }
-
     render() {
         return (
-            <div className='min' onClick={this.change}>
-                <Icon type='user' style={{fontSize: 16, color: '#08c', marginLeft: 16, marginRight: 16}}/>
-                <div>Chat</div>
+            <div className='min'  onClick={this.imdata.openContectList}>
+                 <Icon type='user' style={this.imdata.setIconStyle}/>
+                 <div>Let’s Chat </div>
             </div>
         )
     }
